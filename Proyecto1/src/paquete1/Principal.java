31adf5ea1e75b74f5ec41b022a1543f270fd9302
DAW <DAW@DESKTOP-U4FTC0L>
2024-01-16 13:09:45 +0100

    Final proyecto

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Ejemplo de control de versiones con Git");
 		System.out.println("Este e el 2 Ejemplo de control de versiones con Git");
+		System.out.println("tercera fase");
 	}
 
 }
